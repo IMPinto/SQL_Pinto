3a3d32dbee5079a122b53ed69deed13767fded8f
Israel Pinto <hartl123452019@gmail.com>
2025-06-24 15:34:15 +0300

    fix: apply feedback from code review

--- SQL first part.sql
+++ SQL first part.sql
@@ -1,9 +1,21 @@
+CREATE SEQUENCE city_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE city (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    area VARCHAR(100)
+);
+
+CREATE SEQUENCE school_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE school (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     city_id INT,
     FOREIGN KEY (city_id) REFERENCES city(id)
 );
+
+CREATE SEQUENCE student_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE student (
     id INT PRIMARY KEY,
@@ -13,149 +25,207 @@ CREATE TABLE student (
     FOREIGN KEY (school_id) REFERENCES school(id)
 );
 
-CREATE TABLE examination (
+CREATE TABLE exams (
     id INT PRIMARY KEY,
     subject VARCHAR(100),
     student_id INT,
-    creation_date DATE,
+    test_date DATE,
     school_id INT,
     grade NUMBER(3),
     FOREIGN KEY (student_id) REFERENCES student(id),
     FOREIGN KEY (school_id) REFERENCES school(id)
 );
+-- INSERTS--
+-- Inserts citys--
+INSERT INTO city (id, name, area)
+VALUES (city_seq.NEXTVAL, 'ירושלים', 'מרכז');
 
-CREATE TABLE city (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    area VARCHAR(100)
-);
+INSERT INTO city (id, name, area)
+VALUES (city_seq.NEXTVAL, 'תל אביב', 'מרכז');
 
+INSERT INTO city (id, name, area)
+VALUES (city_seq.NEXTVAL, 'חיפה', 'צפון');
 
--- 1 --
+INSERT INTO city (id, name, area)
+VALUES (city_seq.NEXTVAL, 'באר שבע', 'דרום');
+
+-- Inserts schools--
+INSERT INTO school (id, name, city_id)
+VALUES (school_seq.NEXTVAL, 'תיכון הרצליה', 1);
+
+INSERT INTO school (id, name, city_id)
+VALUES (school_seq.NEXTVAL, 'תיכון בליך', 2);
+
+INSERT INTO school (id, name, city_id)
+VALUES (school_seq.NEXTVAL, 'מקיף חיפה', 3);
+
+INSERT INTO school (id, name, city_id)
+VALUES (school_seq.NEXTVAL, 'מקיף ד באר שבע', 4);
+
+-- Inserts students--
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'שירה', 'כהן', 3);
+
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'דוד', 'לוי', 4);
+
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'נועה', 'ישראלי', 1);
+
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'יואב', 'ברק', 2);
+
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'טל', 'אוחנה', 3);
+
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'רוני', 'כהן', 1);
+
+-- Inserts exams--
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (1, 'מתמטיקה', 1, TO_DATE('2025-06-01', 'YYYY-MM-DD'), 3, 88);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (2, 'אנגלית', 2, TO_DATE('2025-06-02', 'YYYY-MM-DD'), 4, 92);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (3, 'מדעים', 3, TO_DATE('2025-06-03', 'YYYY-MM-DD'), 1, 75);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (4, 'היסטוריה', 4, TO_DATE('2025-06-05', 'YYYY-MM-DD'), 2, 90);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (5, 'מתמטיקה', 5, TO_DATE('2025-06-06', 'YYYY-MM-DD'), 3, 85);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (6, 'עברית', 6, TO_DATE('2025-06-07', 'YYYY-MM-DD'), 1, 78);
+
+-- Simple Queries --
+-- a --
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM student;
 
--- 2 --
+-- b --
 SELECT name
 FROM school;
 
--- 3 --
+-- c --
 SELECT grade, subject
-FROM examination
-WHERE grade >= 55;
+FROM exams
+WHERE grade > 55;
 
--- 4 --
+-- d --
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM student
 WHERE last_name LIKE 'ב%';
 
--- 5 -- 
+-- e -- 
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM student
 ORDER BY first_name;
 
 -- JOINS -- 
--- 6 --
+-- a --
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM student std
 INNER JOIN school scl
 ON std.SCHOOL_ID = scl.ID
-ORDER BY scl.name, CONCAT(std.first_name, ' ', std.last_name);
+ORDER BY LOWER(scl.name), LOWER(full_name);
 
--- 7 -- 
-SELECT creation_date
-FROM examination
-WHERE TO_DATE('1995-01-01', 'YYYY-MM-DD') < CREATION_DATE;
+-- b -- 
+SELECT test_date, grade
+FROM exams
+WHERE TO_DATE('1995-01-01', 'YYYY-MM-DD') < test_date;
 
--- 8 --
+-- c --
 SELECT CONCAT(st.first_name, ' ', st.last_name) AS full_name
 FROM student st
 INNER JOIN school sc
 ON st.SCHOOL_ID = sc.ID
 INNER JOIN city cty
 ON sc.CITY_ID = cty.ID
-WHERE cty.NAME = 'חיפה';
+WHERE cty.NAME = 'חיפה' AND sc.name = 'הריאלי';
 
--- 9 --
+-- d --
 SELECT sc.NAME
 FROM school sc
 INNER JOIN city cty
 ON sc.CITY_ID = cty.ID
-WHERE cty.AREA = 'מרכז';
+WHERE LOWER(cty.AREA) = 'מרכז';
 
--- 10 --
+-- e --
 SELECT sc.NAME, cty.AREA, cty.NAME AS city_name
 FROM school sc
 INNER JOIN city cty
 ON sc.CITY_ID = cty.ID;
 
--- 11 --
+-- f --
 SELECT CONCAT(st.first_name, ' ', st.last_name) AS full_name,
        AVG(ex.grade) AS average_grade
 FROM student st
-INNER JOIN examination ex ON st.id = ex.student_id
+INNER JOIN exams ex ON st.id = ex.student_id
 WHERE st.first_name = 'דפנה'
 GROUP BY st.first_name, st.last_name;
 
--- 12 --
+-- g --
 SELECT CONCAT(s.first_name, ' ', s.last_name) AS full_name,
        sch.name AS school_name,
        ex.subject,
        ex.grade
-FROM examination ex
+FROM exams ex
 JOIN student s ON s.id = ex.student_id
 JOIN school sch ON sch.id = s.school_id
 JOIN city c ON sch.city_id = c.id
 WHERE c.area = 'צפון';
 
--- 13 --
+-- GROUP BY -- 
+-- a --
 SELECT CONCAT(s.first_name, ' ', s.last_name) AS full_name,
        AVG(ex.grade) AS average_grade
-FROM examination ex
+FROM exams ex
 JOIN student s ON s.id = ex.student_id
 JOIN school sch ON sch.id = s.school_id
 WHERE sch.name = 'תיכון לאומנויות'
 GROUP BY s.id, s.first_name, s.last_name;
 
-
--- 14 --
+-- b --
 SELECT sch.name AS school_name,
        AVG(ex.grade) AS average_grade
-FROM examination ex
+FROM exams ex
 JOIN school sch ON ex.school_id = sch.id
 GROUP BY sch.id, sch.name;
 
-
--- 15 --
+-- c --
 SELECT AVG(ex.grade) AS biology_avg
-FROM examination ex
+FROM exams ex
 JOIN school sch ON ex.school_id = sch.id
 WHERE ex.subject = 'ביולוגיה'
   AND sch.name = 'העירוני א';
 
--- 16 -- 
+-- d -- 
 SELECT sch.name AS school_name,
        AVG(ex.grade) AS chemistry_avg
-FROM examination ex
+FROM exams ex
 JOIN school sch ON ex.school_id = sch.id
 WHERE ex.subject = 'כימיה'
 GROUP BY sch.id, sch.name
 ORDER BY chemistry_avg DESC;
 
+-- e --
 
--- 18 -- 
+-- f -- 
 SELECT CONCAT(s.first_name, ' ', s.last_name) AS full_name, 
        AVG(e.grade) AS avg_grade
 FROM student s
-JOIN examination e ON s.id = e.student_id
+JOIN exams e ON s.id = e.student_id
 GROUP BY s.id, s.first_name, s.last_name
 ORDER BY avg_grade DESC
 FETCH FIRST 1 ROWS ONLY;
 
--- 19 --
+-- g --
 SELECT CONCAT(s.first_name, ' ', s.last_name) AS full_name,
        AVG(e.grade) AS avg_math
-FROM examination e
+FROM exams e
 JOIN student s ON e.student_id = s.id
 JOIN school sc ON s.school_id = sc.id
 WHERE e.subject = 'מתמטיקה' AND sc.name = 'העירוני ב'
@@ -163,7 +233,7 @@ GROUP BY s.id, s.first_name, s.last_name
 ORDER BY avg_math DESC
 FETCH FIRST 1 ROWS ONLY;
 
--- 20 --
+-- h --
 SELECT school_name, MAX(avg_grade) AS max_avg
 FROM (
     SELECT sc.name AS school_name,
@@ -171,25 +241,39 @@ FROM (
            AVG(e.grade) AS avg_grade
     FROM student s
     JOIN school sc ON s.school_id = sc.id
-    JOIN examination e ON s.id = e.student_id
+    JOIN exams e ON s.id = e.student_id
     GROUP BY sc.name, s.id
 )
 GROUP BY school_name
 ORDER BY max_avg ASC;
 
--- 21 --
-DELETE FROM examination
+-- INSERT, UPDATE, DELETE --
+-- a --
+INSERT INTO student (id, first_name, last_name, school_id)
+VALUES (student_seq.NEXTVAL, 'עמית', 'זיו', 2);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (7, 'פיזיקה', 7, TO_DATE('2025-06-10', 'YYYY-MM-DD'), 2, 89);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (8, 'ספרות', 7, TO_DATE('2025-06-12', 'YYYY-MM-DD'), 2, 93);
+
+INSERT INTO exams (id, subject, student_id, test_date, school_id, grade)
+VALUES (9, 'כימיה', 7, TO_DATE('2025-06-14', 'YYYY-MM-DD'), 2, 85);
+
+-- b --
+DELETE FROM exams
 WHERE subject = 'אנגלית' AND grade < 40;
 
--- 22 --
-UPDATE examination
+-- c --
+UPDATE exams
 SET grade = CASE 
     WHEN grade - 3 < 0 THEN 0
     ELSE grade - 3
 END
 WHERE subject = 'מתמטיקה';
 
--- 23 --
+-- d --
 CREATE OR REPLACE VIEW northern_schools AS
 SELECT sc.*
 FROM school sc
